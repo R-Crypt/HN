542f87cc6baaaf83a92179e46cd79604b19abe72
R-Crypt <68041569+R-Crypt@users.noreply.github.com>
2025-01-03 22:16:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('apologyButton').addEventListener('click', () => {
+  const message = document.getElementById('message');
+  // Add the 'visible' class to reveal the message
+  message.classList.add('visible');
+});
